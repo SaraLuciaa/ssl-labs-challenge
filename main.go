27a58b5d895e89811606b1440b688d211b8507ba
Sara Lucia <saludipu71@gmail.com>
2026-01-08 22:00:56 -0500

    feat: initialize project structure with database connection and environment loading

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/SaraLuciaa/ssl-labs-challenge/initializers"
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDatabase()
+}
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run()
+}
