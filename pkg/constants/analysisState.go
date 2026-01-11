5e5088fc8eca583076f0f77aca1e3b52bca0c493
Sara Lucia <saludipu71@gmail.com>
2026-01-11 14:10:33 -0500

    refactor: remove analysis state management implementation

--- pkg/constants/analysisState.go
+++
@@ -1,8 +0,0 @@
-package constants
-
-const (
-	AnalysisStatusDNS        = "DNS"
-	AnalysisStatusInProgress = "IN_PROGRESS"
-	AnalysisStatusReady      = "READY"
-	AnalysisStatusError      = "ERROR"
-)
